a74ac42f52468db22b33ad14393f48e20a84d98a
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-08-09 03:34:37 +0100

    Reddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
+"""Function that returns the total no of Reddit subscribers
+    return 0 if no subreddit is passed
+    """
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
+    """Connecting to the Reddit url to get the total sub."""
+
     headers = {'User-Agent': 'reddit_app'}
-    url = f'https://www.reddit.com/r/{sys.argv[1]}/about.json'
-    response = requests.get(url, headers=headers)
-    data = len(response.json())
-    return data
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    try:
+        response = requests.get(url, headers=headers)
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        err = response.raise_for_status()
+    except Exception as err:
+        return (0)
+    return subscribers
